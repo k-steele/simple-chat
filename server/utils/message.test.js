5506143627c5c25a5a22144e98c0e3d58f2bd37e
Kyle Steele <ksteele255@gmail.com>
2017-10-09 22:27:59 -0500

    Add some geolocation stuff, sure, why not

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the expected message', () => {
@@ -8,6 +8,19 @@ describe('generateMessage', () => {
     var message = generateMessage(from, text);
 
     expect(message).toInclude({from, text});
-    expect(message.createdAt, 'number').toBeA('number');
+    expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate the correct location object', () => {
+    var from = 'Kyle';
+    var latitude = 1;
+    var longitude = 2;
+    var url = `https://www.google.com/maps?q=1,2`;
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,  url});
   });
 });
