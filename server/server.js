7394ea941ecf17f2a478740c2f2550f31e6ab36f
Darwin Froese <froese.darwin@gmail.com>
2018-03-04 10:06:29 -0600

    Removed unused libraries

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const namer = require('sillyname');
+
 const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
